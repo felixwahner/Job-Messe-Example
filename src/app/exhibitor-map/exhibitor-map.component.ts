12fa277328f7755dfa2d9641f105aa85956f0aae
Felix Wahner <felix@o315.de>
2023-11-19 13:43:11 +0100

    Include real map image, adjust map scaling

--- src/app/exhibitor-map/exhibitor-map.component.ts
+++ src/app/exhibitor-map/exhibitor-map.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Exhibitor } from '../shared/models/exhibitor';
+import { Exhibitor } from './../shared/models/exhibitor';
+import { Component, effect } from '@angular/core';
 import * as L from 'leaflet';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { Location } from '@angular/common';
 import { ExhibitorsService } from '../shared/services/exhibitors.service';
-import { Observable } from 'rxjs';
 import { IonContent } from '@ionic/angular/standalone';
 import { AppHeaderComponent } from '../shared/components/app-header/app-header.component';
 
@@ -14,63 +13,101 @@ import { AppHeaderComponent } from '../shared/components/app-header/app-header.c
 	standalone: true,
 	imports: [LeafletModule, IonContent, AppHeaderComponent],
 })
-export class ExhibitorMapComponent implements OnInit {
+export class ExhibitorMapComponent {
 	constructor(
 		private location: Location,
 		private exhibitorsService: ExhibitorsService
-	) {}
+	) {
+		effect(() => {
+			if (this.exhibitorsService.get().length) {
+				this.handleDataUpdate();
+			}
+		});
+	}
 	public isMapReady: boolean = false;
 	public map: L.Map | null = null;
-	public markers: Map<string, L.Marker> = new Map();
-
-	public exhibitors: Array<Exhibitor> = this.exhibitorsService.get();
+	public currentMarkers: Map<string, L.Marker> = new Map();
 
 	public leafletOptions = {
-		minZoom: -5,
-		center: L.latLng(5000, -5000),
+		minZoom: 1,
+		maxZoom: 4,
+		center: L.latLng(0, 0),
+		maxBoundsViscosity: 1,
 		crs: L.CRS.Simple,
 	};
 
-	public ngOnInit(): void {}
 	public onMapReady(map: L.Map): void {
 		this.isMapReady = true;
 		this.map = map;
-		const topLeft = L.latLng(0, 0);
-		const bottomRight = L.latLng(10000, 10000);
-		const bounds = L.latLngBounds(topLeft, bottomRight);
-		const url = this.location
-			.prepareExternalUrl('assets/images/backgrounds/example.png')
+		/*
+		To get the correct scaling, we set the bounds
+		to the inital zoom level:
+		zoom 4 6456 * 5120 
+		zoom 3 3228 * 2560 (original size)
+		zoom 2 1614 * 1280
+		zoom 1 807 * 640
+		*/
+		const bounds: L.LatLngBounds = L.latLngBounds([
+			[0, 0],
+			[640, 807], // image size
+		]);
+		this.map.setMaxBounds(new L.LatLngBounds([0, 0], [640, 807]));
+		this.map.fitBounds(bounds);
+		const url: string = this.location
+			.prepareExternalUrl('assets/images/backgrounds/eg.png')
 			.replace('#', '');
 		L.imageOverlay(url, bounds).addTo(this.map as L.Map);
-		this.map.fitBounds(bounds);
-		this.removeMarkersFromMap();
-		this.addMarkersToMap();
+		if (
+			!this.currentMarkers.entries &&
+			this.exhibitorsService.get().length
+		) {
+			this.handleDataUpdate();
+		}
 	}
-	private addMarkersToMap(): void {
+
+	private handleDataUpdate(): void {
 		if (!this.isMapReady) return;
-		(this.exhibitors as Array<Exhibitor>).forEach((exhibitor) => {
-			if (this.markers.get(exhibitor.id) !== undefined) return;
-			const exhibitorPosition = new L.LatLng(
-				exhibitor.coordinates.latitude,
-				exhibitor.coordinates.longitude
+		const newMarkers: Array<Exhibitor> = this.exhibitorsService.get();
+		const markersToRemove: Array<string> = [];
+		this.currentMarkers.forEach((_, id) => {
+			const isIncludedInNewMarkers: boolean = newMarkers.some(
+				(newMarker) => newMarker.id === id
 			);
-			const marker = L.marker(exhibitorPosition)
-				.setLatLng(exhibitorPosition)
-				.addTo(this.map as L.Map)
-				.bindPopup(
-					`<p>${exhibitor.name}<br/><small>${exhibitor.jobTypes.join(
-						','
-					)}</small></p>`,
-					{ autoClose: false, closeOnClick: false }
-				)
-				.openPopup();
-			this.markers.set(exhibitor.id, marker);
+			if (!isIncludedInNewMarkers) {
+				markersToRemove.push(id);
+			}
 		});
+		markersToRemove.forEach((id) => this.removeMarkerFromMap(id));
+		newMarkers.forEach((exhibitor) => this.addMarkerToMap(exhibitor));
 	}
-	private removeMarkersFromMap(): void {
-		this.markers.forEach((marker, id) => {
+
+	private addMarkerToMap(exhibitor: Exhibitor): void {
+		const existingMarker = this.currentMarkers.get(exhibitor.id);
+		if (existingMarker !== undefined) return;
+		const exhibitorPosition = new L.LatLng(
+			exhibitor.coordinates.latitude,
+			exhibitor.coordinates.longitude
+		);
+		const marker = L.marker(exhibitorPosition)
+			.setLatLng(exhibitorPosition)
+			.addTo(this.map as L.Map)
+			.bindPopup(
+				`<p>${exhibitor.name}<br/><small>${exhibitor.jobTypes.join(
+					','
+				)}</small></p>`,
+				{ autoClose: false, closeOnClick: false }
+			)
+			.openPopup();
+		this.currentMarkers.set(exhibitor.id, marker);
+	}
+
+	private removeMarkerFromMap(exhibitorId: string): void {
+		const marker = this.currentMarkers.get(exhibitorId);
+		if (!marker) return;
+		marker.closePopup();
+		if (this.map?.hasLayer(marker)) {
 			this.map?.removeLayer(marker);
-			this.markers.delete(id);
-		});
+		}
+		this.currentMarkers.delete(exhibitorId);
 	}
 }
